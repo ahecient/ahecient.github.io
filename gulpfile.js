b76bf32b11deebe5be7799abad8f62d8e350a6d2
ahecient <48700087+ahecient@users.noreply.github.com>
2019-03-19 02:58:25 +0200

    lab1

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var csso = require('gulp-csso'); 
+var rename = require("gulp-rename");
+var scss = require('gulp-sass');
+
+gulp.task('minCss', function () {
+    return gulp.src('app/css/style.css')    
+       .pipe(csso()) 
+       .pipe(rename({ suffix: ".min" })) 
+       .pipe(gulp.dest('./app/css')); 
+});
+
+gulp.task('test1', function(){
+    return gulp.src('./app/sсss/*.scss') 
+    .pipe(scss())
+    .pipe(gulp.dest('./app/css'));
+});
+ //до запуска вотс базовое обновление файла
+gulp.task('watch', gulp.series (['test1'],['minCss'], function(){
+    gulp.watch('./app/sсss/*.scss',gulp.series(['test1'], ['minCss']));
+}));
